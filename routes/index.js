8398896e3caaad4afdf43de6e755fe9083a3d641
Philip Shanks <pshanks@lab-msi01.hsd1.ca.comcast.net>
2017-10-21 19:23:30 -0700

    Committer: Philip Shanks <pshanks@lab-msi01.hsd1.ca.comcast.net> modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// GET welcome to openshift demo page
+router.get('/welcome', function(req,res) {
+  res.render('welcome',
+  {title: 'the OpenShift Demo',
+  date: 'today\'s date'})
+})
+
 module.exports = router;
